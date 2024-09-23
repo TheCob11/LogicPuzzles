6817a2296b92abb0f879d0293059ad9ee83526bd
Lean 4 VS Code Extension <>
2024-09-23 10:15:18 -0400

    Initial commit

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "LogicPuzzles" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «LogicPuzzles» where
+  -- add any library configuration options here

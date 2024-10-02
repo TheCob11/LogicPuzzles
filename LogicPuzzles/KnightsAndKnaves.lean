d245ce4f6419ff5494adcdd47f650b588029f95b
Cob <25969162+TheCob11@users.noreply.github.com>
2024-10-02 10:09:01 -0400

    huge overhaul of QuestionProblem to work nicer with proofs and especially with SolutionN

--- LogicPuzzles/KnightsAndKnaves.lean
+++ LogicPuzzles/KnightsAndKnaves.lean
@@ -6,82 +6,64 @@ structure Guard where
   good_door: Bool
 deriving Repr
 
-open Guard
-open QuestionProblem (Spec CountingSolver)
-
 structure Basic where
   a : Guard
   b : Guard
   door_eq_not : a.good_door = !b.good_door
-attribute [simp] Basic.door_eq_not
-@[simp] theorem Basic.doors_ne (b: Basic) : b.a.good_door ≠ b.b.good_door :=
-  b.door_eq_not ▸ Bool.not_ne_self b.b.good_door
-@[simp] theorem Basic.not_both_door_true (b: Basic)
-  (ha: b.a.good_door = true) (hb: b.b.good_door = true) : False :=
-  b.doors_ne <| ha.trans hb.symm
-@[simp] theorem Basic.not_both_door_false (b: Basic)
-  (ha: b.a.good_door = false) (hb: b.b.good_door = false) : False :=
-  b.doors_ne <| ha.trans hb.symm
-@[simp] theorem Basic.not_and_doors (b: Basic) :
- ¬(b.a.good_door ∧ b.b.good_door) := fun ⟨ha, hb⟩ ↦
-  b.doors_ne <| ha.trans hb.symm
 abbrev Basic.guards (b: Basic) := [b.a, b.b]
-abbrev BasicProblem : Spec Basic (.CharPicker 2 Guard Bool) := fun b ↦ {
+
+def BasicSpec : QuestionProblem.Spec Basic (.CharPicker 2 Guard Bool) := fun b ↦ {
   check := fun c ↦ b.guards[c].good_door
-  ask := fun c q ↦ b.guards[c].truther = q b.guards[c]
+  ask := fun c q ↦ b.guards[c].truther = q b
 }
 
-example : BasicProblem.Solution where
-  solver prob := bif prob.ask 0 fun _ ↦ prob.ask 0 good_door then 0 else 1
-  all_valid b := by
-    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, doors_ne⟩ <;>
-    simp
-    all_goals exact Bool.false_eq_true.mp doors_ne
+example : BasicSpec.SolutionN 1 := .of_solver_valid_and_n_asks
+  fun asker ↦ do
+    let ans ← asker 0 fun ⟨⟨c_true, c_good⟩, _, _⟩ ↦ c_true == c_good
+    return cond ans 0 1
+  fun b ↦ by
+    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, door_eq_not⟩ <;>
+    apply And.intro <;>
+    solve | exact door_eq_not | exact rfl
 
-example : BasicProblem.SolutionN 1 where
-  solver asker := do
-    let q ← asker fun prob ↦ (0, fun _ ↦ prob.ask 0 good_door)
-    return bif q then 0 else 1
-  all_valid b := by
-    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, doors_ne⟩ <;>
-    simp [pure, bind, WriterT.mk]
-    all_goals exact Bool.false_eq_true.mp doors_ne
-  asks_le_n b := by
-    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, doors_ne⟩ <;>
-    simp [pure, bind, WriterT.mk, Multiplicative.ofAdd]
+example : BasicSpec.SolutionN 1 := .of_solver_valid_and_n_asks
+  fun asker ↦ do
+    let ans ← asker 0 fun b ↦ (BasicSpec b).ask 0 fun ⟨⟨_, good⟩, _, _⟩ ↦ good
+    return cond ans 0 1
+  fun b ↦ by
+    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, door_eq_not⟩ <;>
+    apply And.intro <;>
+    solve | exact door_eq_not | exact rfl
 
-example : BasicProblem.Solution where
-  solver prob := bif prob.ask 0 fun ⟨char, good⟩ ↦ (char = true) = good then 0 else 1
-  all_valid b := by
-    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, doors_ne⟩ <;>
-    simp
-    all_goals exact Bool.false_eq_true.mp doors_ne
+example : BasicSpec.SolutionN 1 := .of_solver_valid_and_n_asks
+  fun asker ↦ do
+    let ans ← asker 0 fun ⟨⟨char, good⟩, _, _⟩ ↦ (char = true) = good
+    return cond ans 0 1
+  fun b ↦ by
+    apply And.intro <;>
+    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, door_eq_not⟩ <;>
+    solve | exact door_eq_not | rfl
 
-example : BasicProblem.SolutionN 1 where
-  solver asker := do
-    let q ← asker fun _p ↦ (0, fun ⟨char, good⟩ ↦ (char = true) = good)
-    return bif q then 0 else 1
-  all_valid b := by
-    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, doors_ne⟩ <;>
-    simp [pure, bind, WriterT.mk] at doors_ne ⊢
-  asks_le_n b := by
-    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, doors_ne⟩ <;>
-    simp [pure, bind, WriterT.mk, Multiplicative.ofAdd]
-
-example (chars_ne: (b: Basic) → b.a.truther ≠ b.b.truther) : BasicProblem.Solution where
-  solver prob := bif prob.ask 0 fun _ ↦ prob.ask 1 good_door then 0 else 1
-  all_valid b := by
+example (chars_ne: (b: Basic) → b.a.truther ≠ b.b.truther) :
+  BasicSpec.SolutionN 1 := .of_solver_valid_and_n_asks
+  fun asker ↦ do
+    let ans ← asker 0 fun prob ↦ (BasicSpec prob).ask 1 fun ⟨_, ⟨_, good⟩, _⟩ ↦ good
+    return cond ans 0 1
+  fun b ↦ by
     have := chars_ne b
-    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, doors_ne⟩ <;>
-    simp_all
-    all_goals exact Bool.true_eq_false.mp doors_ne
+    apply And.intro <;>
+    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, door_eq_not⟩ <;>
+    simp [door_eq_not] at this <;>
+    solve | exact door_eq_not | rfl
 
-example (chars_ne: (b: Basic) → b.a.truther ≠ b.b.truther) : BasicProblem.Solution where
-  solver prob :=
-    bif prob.ask 0 fun ⟨char, good⟩ ↦ bif char = true then good else !good
-    then 0 else 1
-  all_valid b := by
+example (chars_ne: (b: Basic) → b.a.truther ≠ b.b.truther) :
+  BasicSpec.SolutionN 1 := .of_solver_valid_and_n_asks
+  fun asker ↦ do
+    let ans ← asker 0 fun ⟨⟨char, good⟩, _, _⟩ ↦ cond char good !good
+    return cond ans 0 1
+  fun b ↦ by
     have := chars_ne b
-    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, doors_ne⟩ <;>
-    simp_all
-    all_goals exact Bool.true_eq_false.mp doors_ne
+    apply And.intro <;>
+    rcases b with ⟨⟨_|_, _|_⟩, ⟨_|_, _|_⟩, door_eq_not⟩ <;>
+    simp [door_eq_not] at this ⊢ <;>
+    solve | exact door_eq_not | rfl
--- LogicPuzzles/QuestionProblem.lean
+++ LogicPuzzles/QuestionProblem.lean
@@ -2,37 +2,37 @@ import Mathlib
 
 structure QuestionProblem.Config where
   (Character Idx Response Solution : Type*)
-abbrev QuestionProblem.Config.Question (cfg: Config) := cfg.Character → Bool
-class QuestionProblem (cfg: QuestionProblem.Config) where
+abbrev QuestionProblem.Question (α: Type*) := α → Bool
+class QuestionProblem (cfg: QuestionProblem.Config) (α: Type*) where
   check : cfg.Solution → Prop
-  ask : cfg.Idx → cfg.Question → cfg.Response
+  ask : cfg.Idx → QuestionProblem.Question α → cfg.Response
 namespace QuestionProblem
-
 abbrev Config.CharPicker (n: ℕ) := (Config.mk · (Fin n) · (Fin n))
+abbrev Config.CharIDer (Character Idx : Type*) :=
+  (Config.mk Character Idx · (Idx → Character))
 
-def Solver (cfg) := QuestionProblem cfg → cfg.Solution
-def valid (self: QuestionProblem cfg) (solver: Solver cfg) : Prop :=
-  self.check (solver self)
+def Spec α cfg := α → QuestionProblem cfg α
+abbrev History α cfg := List (Config.Idx cfg × Question α)
+abbrev Asker M [Monad M] α cfg :=
+  cfg.Idx → Question α → WriterT (History α cfg) M cfg.Response
+abbrev loggedAsk (prob: QuestionProblem cfg α): Asker Id α cfg :=
+  fun i q ↦ (prob.ask i q, [(i, q)])
+def Solver α cfg := {M: Type _ → Type _} → [Monad M] → Asker M α cfg →
+  WriterT (History α cfg) M cfg.Solution
+abbrev Solver.fromSpec (_: Spec α cfg) := Solver α
+namespace Spec
+def valid (spec: Spec α cfg) (solver: Solver α cfg) (a: α): Prop :=
+  (spec a).check <| @solver Id _ (spec a).loggedAsk |>.1
+def n_asks (spec: Spec α cfg) (solver: Solver α cfg) (n: ℕ) (a: α) : Prop :=
+  @solver Id _ (spec a).loggedAsk |>.2.length = n
+structure Solution (self: Spec α cfg) where
+  solver: Solver α cfg
+  valid a: self.valid solver a
+structure SolutionN (self: Spec α cfg) (n: ℕ) extends Solution self where
+  n_asks a: self.n_asks solver n a
+def SolutionN.of_solver_valid_and_n_asks {spec: Spec α cfg} {n: ℕ}
+  (solver: Solver α cfg) (h: ∀a, spec.valid solver a ∧ spec.n_asks solver n a) :
+  spec.SolutionN n := ⟨⟨solver, (h · |>.left)⟩, (h · |>.right)⟩
 
-def Spec (α: Type*) (cfg) := α → QuestionProblem cfg
-abbrev Spec.Problem (_: Spec α cfg) := QuestionProblem cfg
--- this doesn't just take in a problem because if it did the solver could
--- know about it
-structure Spec.Solution (self: Spec α cfg) where
-  solver : Solver cfg
-  all_valid : ∀a: α, (self a).valid solver
-
-def CountingAsker (cfg: Config) {M} [Monad M] :=
-  (QuestionProblem cfg → cfg.Idx × cfg.Question) →
-    WriterT (Multiplicative ℕ) M cfg.Response
-def CountingSolver (cfg: Config) := {M: Type → Type} → [Monad M] →
-    @CountingAsker cfg M _ → WriterT (Multiplicative ℕ) M cfg.Solution
-abbrev CountingSolver.run (self: CountingSolver cfg) (prob: QuestionProblem cfg) :
-  Writer (Multiplicative ℕ) cfg.Solution :=
-  self (prob.ask.uncurry <| · prob, .ofAdd 1)
-structure Spec.SolutionN (spec: Spec α cfg) (n: ℕ) where
-  solver : CountingSolver cfg
-  all_valid : ∀a: α, (spec a).check (solver.run (spec a)).1
-  asks_le_n : ∀a: α, (solver.run (spec a)).2 ≤ n
-
-attribute [simp] check ask valid CountingSolver.run
+end Spec
+attribute [simp] check ask loggedAsk Spec.valid Spec.n_asks
